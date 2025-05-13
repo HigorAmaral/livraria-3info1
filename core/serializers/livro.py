75cbbe807add78be172cb7f7f15968c1e4459a16
higoramaral <amarahigor@gmail.com>
2025-05-13 16:45:24 -0300

    feat: criação de multiplos serializers  para livro

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -6,4 +6,10 @@ class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = "__all__"
-        read_only_fields = ['id']
+
+
+class LivroListRetrieveSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1
